bd1467fb741b32f2ecfd6b3d77088bcd94d72ef6
Sivarajan <citysiva180@users.noreply.github.com>
2024-03-12 06:57:32 +0530

    styling components complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import { useState } from "react";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
+import Card from "./components/shared/Card";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackItem></FeedbackItem>
+        <FeedbackList feedback={feedback}></FeedbackList>
       </div>
     </>
   );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,17 @@
-function FeedbackItem() {
+import Card from "./shared/Card";
+import PropTypes from "prop-types";
+
+function FeedbackItem({ item }) {
   return (
-    <div className="card">
-      <div className="num-display">10</div>
-      <div className="text-display">This is an Example Feedback Item</div>
-    </div>
+    <Card reverse={true}>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
+    </Card>
   );
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
